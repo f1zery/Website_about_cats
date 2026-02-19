bda4477dabca49f99646a82ae8571988f0c06aca
f1zery <felik98@inbox.ru>
2026-02-19 13:10:47 +0600

    Add files via upload

--- js/photos.js
+++ js/photos.js
@@ -4,17 +4,38 @@
    Поля:
    - src: путь к картинке 
    - alt: описание 
-   - caption: подпись под фото
-   - authorName: имя автора
-   - authorUrl: ссылка на автора 
+   - caption: подпись под фото (необязательно)
 */
 
 window.CAT_PHOTOS = [
   {
-    src: "image/3b44410d4cb6383629b66f7d451f58a4-Photoroom 1.png",
-    alt: "Смешной кот с хлебом на голове",
-    caption: "Кот-сэндвич",
-    authorName: "Автор неизвестен",
-    authorUrl: "",
+    src: "image/cats/1.jpg",
+    alt: "Кот с закрытыми глазами",
+    sourceAuthor: "Karsten Paulick, Pixabay",
+  },
+  {
+    src: "image/cats/2.jpg",
+    alt: "Кот лежит",
+    sourceAuthor: 'Изображение <a href="https://pixabay.com/ru/users/kapa65-61253/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5219187">Karsten Paulick</a> с сайта <a href="https://pixabay.com/ru//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5219187">Pixabay</a>'
+  },
+  {
+    src: "image/cats/3.jpg",
+    alt: "Кот зевает",
+    sourceAuthor: 'Изображение <a href="https://pixabay.com/ru/users/kapa65-61253/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=814141">Karsten Paulick</a> с сайта <a href="https://pixabay.com/ru//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=814141">Pixabay</a>'
+  },
+  {
+    src: "image/cats/4.jpg",
+    alt: "Кот сидит",
+    sourceAuthor: 'Изображение <a href="https://pixabay.com/ru/users/skorchanov-5650284/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3602556">Сергей Корчанов</a> с сайта <a href="https://pixabay.com/ru//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3602556">Pixabay</a>'
+  },
+  {
+    src: "image/cats/5.jpg",
+    alt: "Прыжок",
+    sourceAuthor: 'Изображение <a href="https://pixabay.com/ru/users/rihaij-2145/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1366118">rihaij</a> с сайта <a href="https://pixabay.com/ru//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1366118">Pixabay</a>'
+  },
+  {
+    src: "image/cats/6.jpg",
+    alt: "Кот на скворечнике",
+    sourceAuthor: 'Изображение <a href="https://pixabay.com/ru/users/rihaij-2145/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1400812">rihaij</a> с сайта <a href="https://pixabay.com/ru//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1400812">Pixabay</a>'
   },
 ];
